fe8b388b2029550a4fd4a12e2f729576b6894d64
10mi8o <totoma855@gmail.com>
2020-05-31 08:44:33 +0900

    Navigatorテスト実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,8 +11,50 @@ const styles = StyleSheet.create({
   },
 });
 
+const Main = (): ReactElement => {
+  const { navigate } = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>Main</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigate('Sub');
+        }}
+      >
+        <Text>Go go sub</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Sub = (): ReactElement => {
+  return (
+    <View style={styles.container}>
+      <Text>sub</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+const StackNavigator = (): ReactElement => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{ title: 'Main画面' }}
+      />
+      <Stack.Screen name="Sub" component={Sub} options={{ title: 'Sub画面' }} />
+    </Stack.Navigator>
+  );
+};
+
 const Index = (): ReactElement => {
-  return <View style={styles.container} />;
+  return (
+    <NavigationContainer onStateChange={newState => console.log({ newState })}>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default Index;
