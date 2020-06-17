b7a568c2625c022add57bacdf01afbdf3364a983
10mi8o <totoma855@gmail.com>
2020-06-17 23:31:56 +0900

    Statisticsページ追加

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,3 +2,4 @@ export const INITIAL = 'INITIAL';
 export const HOME = 'HOME';
 export const LOADING = 'LOADING';
 export const CHOOSE_LOGIN = 'CHOOSE_LOGIN';
+export const STATISTICS = 'STATISTICS';
